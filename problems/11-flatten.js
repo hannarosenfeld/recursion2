c5e4c63c75ca323e81fdd4c7ea7f24468bc83ceb
Hanna Rosenfeld <hannazrosenfeld@gmail.com>
2023-02-07 16:46:41 -0500

    11.

--- problems/11-flatten.js
+++ problems/11-flatten.js
@@ -11,11 +11,32 @@ flatten([]); // []
 flatten([1, 2]); // [1, 2]
 flatten([1, [2, [3]]]); // [1, 2, 3]
 ***********************************************************************/
+// base case idea 1: check if array
+// 2 check if og array is empty
 
 function flatten(arr) {
-  // Your code here
+    let newArr = [];
+
+    // check if element is array
+     for (let i = 0; i < arr.length; i++) {
+      if (Array.isArray(arr[i])) {
+        //if so use spread operator to access inside value
+        newArr.push(...flatten(arr[i]));
+      } else {
+        newArr.push(arr[i]);
+      }
+    }
+
+    console.log("newArr: ",newArr)
+    return newArr;
 }
-  
+
+//flatten([]); // []
+//flatten([1, 2]); // [1, 2]
+flatten([1, [2, [3]]]); // [1, 2, 3]
+
+
+
+
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 module.exports = flatten;
-  
